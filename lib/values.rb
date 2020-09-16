340dfaeaff6cceaa883e4bbad23bc4a8d4570481
Taylor Booth <taylorkbooth@gmail.com>
2020-09-16 19:33:53 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,9 +9,7 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie}"
+  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
   
   groceries.values
-  
-  
 end
