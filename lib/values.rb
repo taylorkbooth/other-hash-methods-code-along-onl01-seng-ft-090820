b159c96ba6eb4f8c028ff25bc347d35ef06e4c38
Taylor Booth <taylorkbooth@gmail.com>
2020-09-16 20:03:11 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,10 +10,10 @@ require 'pry'
 
 def get_the_values(groceries)
   groceries = {
-    fruit: "Banana", 
-    vegetable: "Broccoli", 
-    dessert: "Cookie"
-  }
-  
-  groceries.values
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+groceries.values
 end
