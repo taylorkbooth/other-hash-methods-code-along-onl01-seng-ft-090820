600e037e0f7c430859fda940cecb783a1785f386
Taylor Booth <taylorkbooth@gmail.com>
2020-09-16 20:05:16 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -15,5 +15,5 @@ def get_the_values(groceries)
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
 }
-groceries.values(:dairy, :vegetable, :meat, :grains)
+groceries.values(:dairy, :vegetables, :meat, :grains)
 end
